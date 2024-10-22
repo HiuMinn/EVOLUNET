15da6cfc19cb8732ee92320b0fa8680ad8985a62
Dang Hieu Minh <hmdang@MacBook-Air-cua-Dang.local>
2024-10-23 00:48:10 +0200

    add methods

--- network.py
+++ network.py
@@ -1,8 +1,26 @@
 import numpy as np
 
+
+def sigmoid(z):
+    """
+    calculate sigmoid function
+    :param z: vector or Numpy array
+    :return:
+    """
+    return 1 / (1 + np.exp(-z))
 class EVOLUNET():
     def __init__(self, size):
         self.size = size
         self.num_layers = len(self.size)
-        self.bias = 0
-    
+        self.bias = [np.random.randn(y,1) for y in size[1:]]
+        self.weights = [np.random.randn(x,y) for (x,y) in zip(size[:-1],size[1:])
+
+    def feedforward(self, a):
+        """
+        return output of the network if inputs
+        :param a: input vector
+        :return:
+        """
+        for b, w in zip(self.bias, self.weights):
+            a = sigmoid(np.dot(w,a)+b)
+        return a
