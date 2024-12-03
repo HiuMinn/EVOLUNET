0bc75218fb9172ce1899ca92484977984fd78861
hmdang <hieu-minh.dang@insa-lyon.fr>
2024-12-03 09:11:31 +0100

    fix evaluate func

--- evolution.py
+++ evolution.py
@@ -2,66 +2,107 @@
 from network import EVOLUNET
 import matplotlib.pyplot as plt
 import numpy as np
-from PrettyTable import PrettyTable
 import random
 ########################################## genetic algorithm #########################
 class Evolution:
-    def __init__(self, nb_individuals, nb_generations):
+    def __init__(self, sizes,nb_individuals=50, nb_generations=100):
         self.nb_individuals = nb_individuals
         self.nb_generations = nb_generations
-        self.population = []
+        self.neuro_size = sizes
         self.fitness = []
+        self.population = []
+        self.fitness_history = []
+        self.best_fitness_history = []
+        self.tournament_size = 0
 
-    def new_population(self,nb_layers):
-        for i in range(self.nb_individuals):
-            individual = EVOLUNET(nb_layers)
-            self.population.append(individual)
+    def init_population(self,method = "full"):
+        self.population = []
+        self.fitness = [] #init fitness eachtime
+        if method == "full":
+            for i in range(self.nb_individuals):
+                self.population.append(EVOLUNET(self.neuro_size))
+        elif method == "one":
+            self.population.append(EVOLUNET(self.neuro_size))
 
-    def calculate_fitness(self):
-        for individual in self.population:
-            self.fitness.append(individual.evaluate())
+    def calculate_fitness(self,training_data):
+        self.fitness =[] #init fitness
+        i_max = 0
+        max_fitness = 0
+        for i,individual in enumerate(self.population):
+            indi_fitness = individual.evaluate(training_data)
+            #search for the max fitness individual
+            if indi_fitness > max_fitness:
+                i_max= i
+                max_fitness = indi_fitness
+            self.fitness.append(indi_fitness)
+        return i_max,max_fitness
 
     def selection_tournament(self,tournament_size = 3):
         selected = []
-        for i in range(len(self.population)):
-            tournament = random.sample(list(zip(self.population,self.fitness)), tournament_size)
-            winner = max(tournament, key=lambda x: x[1])[0]
+        self.tournament_size = tournament_size
+        for _ in range(len(self.population)):
+            tournament = random.sample(list(zip(self.population,self.fitness)), self.tournament_size)
+            winner = max(tournament, key=lambda x: x[1])
             selected.append(winner)
         return selected
 
+    def plot_fitness(self):
+        fig,ax = plt.subplots()
+        ax.plot(range(len(self.fitness_history)), self.fitness_history, label='Average fitness',linestyle = "-.")
+        ax.plot(range(len(self.best_fitness_history)), self.best_fitness_history, label = "Best fitness",linestyle = ":")
+        ax.set_title(f"fitness along the {self.nb_generations} generations with \n tournament selection upto {self.tournament_size} in a population of {self.nb_individuals} individuals ")
+        ax.set(xlabel="Generation", ylabel=f'negative cost')
+        ax.legend()
+        plt.show()
 
-    def crossover(self,parent1, parent2):
-        alpha = random.random()
-        for w1,w2,b1,b2 in zip(parent1.weights, parent2.weights, parent1.bias, parent2.bias):
-            child1_weights = [alpha*w1[i] + (1-alpha)*w2[i] for i in range(len(w1))]
-            child2_weights = [alpha*w2[i] + (1-alpha)*w1[i] for i in range(len(w1))]
-            child1_bias = [alpha*b1[i] + (1-alpha)*b2[i] for i in range(len(b1))]
-            child2_bias = [alpha*b2[i] + (1-alpha)*b1[i] for i in range(len(b1))]
-        return child1_weights,child2_weights,child1_bias,child2_bias
-    def genetic_algorithm(self,mutation_rate):
-        best_performace = []
+    def get_average_fitness(self):
+        return sum(self.fitness) / len(self.fitness)
+    def reproduce(self,selected,training_data):
+        (parent,parent_fitness)= selected
+        parent_weights = parent.weights
+        parent_bias = parent.bias
+        # create new child with mutated weights and bias
+        child = EVOLUNET(self.neuro_size, parent_bias, parent_weights)
+        child.mutate_param()
+        child_fitness = child.evaluate(training_data)
+        return child,child_fitness,parent, parent_fitness
+
+    def generation_evaluation(self,training_data):
+        self.init_population(method = "full")
 
         for generation in range(self.nb_generations):
-            self.calculate_fitness()
-            best_individual = max(self.fitness)
+            if generation % 10 ==0:
+                print("Generation {}".format(generation))
 
-            sub_population = self.selection_tournament(tournament_size = 3)
+            i_best_individual,best_fitness = self.calculate_fitness(training_data)
+            average_fitness = self.get_average_fitness()
+            # print(best_fitness,average_fitness)
+            #keep information for plot
+            self.fitness_history.append(average_fitness)
+            self.best_fitness_history.append(best_fitness)
+
+            sub_population = self.selection_tournament(tournament_size = 10)
             next_population = []
-            for i in range(0,len(sub_population),2):
-                parent1 = self.population[i]
-                parent2 = self.population[i+1]
-
-                child1_weights, child2_weights, child1_bias, child2_bias = self.crossover(parent1,parent2)
-
-                #create new population with children
-                child1 = EVOLUNET(child1_weights,child1_bias)
-                child2 = EVOLUNET(child2_weights,child2_bias)
-
-                child1.mutate_param(mutation_rate)
-                child2.mutate_param(mutation_rate)
-       
-                next_population.append(child1)
-                next_population.append(child2)
-            next_population[0] = best_individual
+            for i in range(0,len(sub_population)):
+                child, child_fitness, parent, parent_fitness= self.reproduce(sub_population[i],training_data)
+                if child_fitness > parent_fitness: #if new child perform better, keep child, else keep the parent
+                    next_population.append(child)
+                else:
+                    next_population.append(parent)
+            next_population[0] = self.population[i_best_individual]
             self.population = next_population
 
+        print("learning process finished")
+
+        i_optimized_individual,optimized_fitness = self.calculate_fitness(training_data)
+        average_fitness = sum(self.fitness)/len(self.fitness)
+        self.fitness_history.append(average_fitness)
+        self.best_fitness_history.append(optimized_fitness)
+        self.plot_fitness()
+        return self.population[i_optimized_individual] #use the best individual
+
+    # def explose(self,training_data):
+
+if __name__ == "__main__":
+    test = Evolution([2,3,2])
+    test.init_population()
--- network.py
+++ network.py
@@ -1,50 +1,48 @@
 import numpy as np
 import random
+import matplotlib.pyplot as plt
+
 
 
 class EVOLUNET():
     def __init__(self, size,bias = None,weights = None):
+        """
+
+        :param size:
+        :param bias: If no bias given, generate randomly by default
+        :param weights: If no weights given, generate randomly by default
+        """
         self.size = size
         self.num_layers = len(self.size)
-        if bias is None:
-            self.bias = []
+        if bias is None or weights is None:
+            self.new_param()
         else:
-            self.bias =bias
-        if weights is None:
-            self.weights = []
+            self.bias = bias
+            self.weights = weights
 
-    def new_network(self):
+    def new_param(self):
         """
         initiate a new random parameters of the network
         :param self:
         :return:
         """
         self.bias = [np.random.randn(y, 1) for y in self.size[1:]]
-        self.weights = [np.random.randn(x, y) for (x, y) in zip(self.size[:-1], self.size[1:])]
+        self.weights = [np.random.randn(y, x) for x, y in zip(self.size[:-1], self.size[1:])]
+        # self.bias = [np.zeros([y,1]) for y in self.size[1:]]
+        # self.weights = [np.zeros([y, x]) for x, y in zip(self.size[:-1], self.size[1:])]
         return None
 
-    def mutate_param (self,mutation_rate):
+    def mutate_param (self,std_weights = 1, std_bias =1):
         """
-        regarding a neural network as a individual in the population,
+        regarding a neural network as a individual in the population, varie by norminal distribution
         mutate the bias and weights
         :return:
         """
-        for i in range(len(self.bias)):
-            if random.random()< mutation_rate:
-                self.bias[i] += random.uniform(-1,1)
-
         for i in range(len(self.weights)):
-            if random.random()< mutation_rate:
-                self.weights[i] += random.uniform(-0.1,0.1)
-        return None
+            self.weights[i] += np.random.normal(0,std_weights,size = self.weights[i].shape)
+        for i in range(len(self.bias)):
+            self.bias[i] += np.random.normal(0,std_bias,size = self.bias[i].shape)
 
-    def mutate(self):
-        sign_weights = np.random.choice([-1,1], size=self.weights)
-        mutate_weights =
-        sign_bias = np.random.choice([-1,1], size=self.bias)
-        mutate_bias = 0.1
-        self.bias = [self.bias[i] + sign_bias[i]*mutate_bias for i in range(len(self.bias))]
-        self.weights = [self.weights[i] + sign_weights[i]*mutate_weights for i in range(len(self.weights))]
 
     def feedforward(self, a):
         """
@@ -52,7 +50,10 @@ class EVOLUNET():
         :param a: input vector
         :return:
         """
+
         for b, w in zip(self.bias, self.weights):
+            # print(f"a {a}")
+            # print(f"w {w}")
             a = self.sigmoid(np.dot(w,a)+b)
         return a
 
@@ -64,20 +65,52 @@ class EVOLUNET():
         """
         return 1 / (1 + np.exp(-z))
 
-    def evaluate(self, test_data):
+    def evaluate(self, training_data):
         """
         Return the number of correct predictions. Seen as fitness for the GA
+        NB: for classification problems only
         :param test_data:
         :return:
         """
-        test_output = [(self.feedforward(test_data),y) for (x,y) in test_data]
-        return sum(int(x==y) for (x,y) in test_output)
+        # fitness = 0
+        X_train,y_train = training_data
+        y_pred = self.feedforward(X_train.T)
+        # print(np.array(y_pred))
+        # print(y_train)
+        cost = 1/len(X_train)*np.sum(y_train*np.log(y_pred)+(1-y_train)*np.log(1-y_pred))
+        return cost
+
+    def plot_decision_boundary(self,train_X,train_y):
+        x_min, x_max = train_X[:, 0].min(), train_X[:, 0].max()
+        y_min, y_max = train_X[:, 1].min(), train_X[:, 1].max()
+
+        xx ,yy = np.meshgrid(np.linspace(x_min, x_max, 500)
+                             ,np.linspace(y_min, y_max, 500))
+
+        grid = np.c_[xx.ravel(), yy.ravel()]
+
+        prediction = []
+        for point in grid:
+            point = point.reshape(-1,1)
+            output = np.argmax(self.feedforward(point))
+            prediction.append(output)
+        print(prediction)
+        prediction = np.array(prediction).reshape(xx.shape)
+        plt.contourf(xx, yy, prediction, cmap = plt.cm.coolwarm, alpha = 0.6)
+        plt.scatter(train_X[:, 0], train_X[:, 1], c=train_y, cmap=plt.cm.coolwarm, edgecolor='k')
+        plt.xlabel('Feature 1')
+        plt.ylabel('Feature 2')
+        plt.title('Decision Boundary of the Neural Network')
+        plt.show()
 
 
-    # def evaluate_by_matrix(self,test_data):
-    #     test_output = [(self.feedforward(test_data), y) for (x, y) in test_data]
-    #     tp=tn=fn=fp =0
-    #     for (x,y) in test_output:
-    #         tp += int(x==y)
-    #         fp += int(x!=y)
-    #     return 0
+if __name__ == '__main__':
+    network  = EVOLUNET(size=[2,3,2,1])
+    network.new_param()
+
+    print("old weights:",network.weights)
+    print("old bias",network.bias)
+
+    network.mutate_param()
+    print("new weights: ",network.weights)
+    print("new bias: ", network.bias)
